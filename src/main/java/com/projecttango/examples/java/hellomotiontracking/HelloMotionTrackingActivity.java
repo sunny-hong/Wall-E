684f32fa6ecdd0efc5a4ad668ba6ea82c58a4dd4
mdnahian <mdnahian@outlook.com>
2016-10-08 07:51:05 -0400

    First Commit

---
+++ src/main/java/com/projecttango/examples/java/hellomotiontracking/HelloMotionTrackingActivity.java
@@ -0,0 +1,325 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.projecttango.examples.java.hellomotiontracking;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPointCloudData;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ * Main Activity class for the Motion Tracking API Sample. Handles the connection to the Tango
+ * service and propagation of Tango pose data Layout view.
+ */
+public class HelloMotionTrackingActivity extends Activity {
+
+    private static final String TAG = HelloMotionTrackingActivity.class.getSimpleName();
+
+    private Tango mTango;
+    private TangoConfig mConfig;
+    private TextView position;
+    private TextView o;
+
+    private Button recordBtn;
+    private Boolean isRecording = false;
+
+    private ListView destinations;
+
+    ArrayList<float[]> positions = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motion_tracking);
+
+        position = (TextView) findViewById(R.id.position);
+        o = (TextView) findViewById(R.id.orientation);
+        recordBtn = (Button) findViewById(R.id.record);
+
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isRecording){
+                    stopRecording();
+                    isRecording = false;
+                    recordBtn.setText("Record");
+                    showDialog();
+                } else {
+                    startRecording();
+                    isRecording = true;
+                    recordBtn.setText("Stop");
+                }
+            }
+        });
+
+        destinations = (ListView) findViewById(R.id.destinations);
+
+    }
+
+
+
+    public class DistinationsAdapter extends ArrayAdapter<float[]>{
+
+        public DistinationsAdapter(){
+            super(getApplicationContext(), android.R.layout.simple_list_item_2, positions);
+        }
+
+    }
+
+
+
+
+    public void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Set a Destination");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                saveToInternalStorage(input.getText().toString());
+
+                ArrayList<float[]> sample = readFromInternalStorage(input.getText().toString());
+                Log.d("Crash", sample.toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void saveToInternalStorage(String name) {
+        try {
+            FileOutputStream fos = this.openFileOutput(name, Context.MODE_PRIVATE);
+            ObjectOutputStream of = new ObjectOutputStream(fos);
+            of.writeObject(positions);
+            of.flush();
+            of.close();
+            fos.close();
+        }
+        catch (Exception e) {
+            Log.e("InternalStorage", e.getMessage());
+        }
+    }
+
+    public ArrayList<float[]> readFromInternalStorage(String name) {
+        ArrayList<float[]> toReturn = new ArrayList<>();
+        FileInputStream fis;
+        try {
+            fis = this.openFileInput(name);
+            ObjectInputStream oi = new ObjectInputStream(fis);
+            toReturn = (ArrayList<float[]>) oi.readObject();
+            oi.close();
+        } catch (FileNotFoundException e) {
+            Log.e("InternalStorage", e.getMessage());
+        } catch (IOException e) {
+            Log.e("InternalStorage", e.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("InternalStorage", e.getMessage());
+        }
+
+        return toReturn;
+    }
+
+    private void startRecording(){
+        mTango = new Tango(HelloMotionTrackingActivity.this, new Runnable() {
+            // Pass in a Runnable to be called from UI thread when Tango is ready, this Runnable
+            // will be running on a new thread.
+            // When Tango is ready, we can call Tango functions safely here only when there is no UI
+            // thread changes involved.
+            @Override
+            public void run() {
+                synchronized (HelloMotionTrackingActivity.this) {
+                    mConfig = setupTangoConfig(mTango);
+
+                    try {
+                        setTangoListeners();
+                    } catch (TangoErrorException e) {
+                        Log.e(TAG, getString(R.string.exception_tango_error), e);
+                    } catch (SecurityException e) {
+                        Log.e(TAG, getString(R.string.permission_motion_tracking), e);
+                    }
+                    try {
+                        mTango.connect(mConfig);
+                    } catch (TangoOutOfDateException e) {
+                        Log.e(TAG, getString(R.string.exception_out_of_date), e);
+                    } catch (TangoErrorException e) {
+                        Log.e(TAG, getString(R.string.exception_tango_error), e);
+                    }
+                }
+            }
+        });
+    }
+
+    private void stopRecording(){
+        synchronized (this) {
+            try {
+                mTango.disconnect();
+            } catch (TangoErrorException e) {
+                Log.e(TAG, getString(R.string.exception_tango_error), e);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Initialize Tango Service as a normal Android Service, since we call mTango.disconnect()
+        // in onPause, this will unbind Tango Service, so every time when onResume gets called, we
+        // should create a new Tango object.
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    /**
+     * Sets up the tango configuration object. Make sure mTango object is initialized before
+     * making this call.
+     */
+    private TangoConfig setupTangoConfig(Tango tango) {
+        // Create a new Tango Configuration and enable the HelloMotionTrackingActivity API.
+        TangoConfig config = new TangoConfig();
+        config = tango.getConfig(config.CONFIG_TYPE_DEFAULT);
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+
+        // Tango service should automatically attempt to recover when it enters an invalid state.
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_AUTORECOVERY, true);
+        return config;
+    }
+
+    /**
+     * Set up the callback listeners for the Tango service, then begin using the Motion
+     * Tracking API. This is called in response to the user clicking the 'Start' Button.
+     */
+    private void setTangoListeners() {
+        // Lock configuration and connect to Tango
+        // Select coordinate frame pair
+        final ArrayList<TangoCoordinateFramePair> framePairs =
+                new ArrayList<TangoCoordinateFramePair>();
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        // Listen for new Tango data
+        mTango.connectListener(framePairs, new OnTangoUpdateListener() {
+            @Override
+            public void onPoseAvailable(final TangoPoseData pose) {
+//                logPose(pose);
+
+                HelloMotionTrackingActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            float translation[] = pose.getTranslationAsFloats();
+                            positions.add(translation);
+                            position.setText(stringBuilder.append("Position: " + translation[0] + ", " + translation[1] + ", " + translation[2]));
+
+                            stringBuilder = new StringBuilder();
+                            float orientation[] = pose.getRotationAsFloats();
+                            o.setText(stringBuilder.append("Orientation: " + orientation[0] + ", " + orientation[1] + ", " +
+                                    orientation[2] + ", " + orientation[3]));
+                    }
+                });
+
+            }
+
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
+                // We are not using onXyzIjAvailable for this app.
+            }
+
+            @Override
+            public void onPointCloudAvailable(TangoPointCloudData pointCloud) {
+                // We are not using onPointCloudAvailable for this app.
+            }
+
+            @Override
+            public void onTangoEvent(final TangoEvent event) {
+                // Ignoring TangoEvents.
+            }
+
+            @Override
+            public void onFrameAvailable(int cameraId) {
+                // We are not using onFrameAvailable for this application.
+            }
+        });
+    }
+
+    /**
+     * Log the Position and Orientation of the given pose in the Logcat as information.
+     *
+     * @param pose the pose to log.
+     */
+    private void logPose(TangoPoseData pose) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        float translation[] = pose.getTranslationAsFloats();
+        stringBuilder.append("Position: " +
+                translation[0] + ", " + translation[1] + ", " + translation[2]);
+
+        float orientation[] = pose.getRotationAsFloats();
+        stringBuilder.append(". Orientation: " +
+                orientation[0] + ", " + orientation[1] + ", " +
+                orientation[2] + ", " + orientation[3]);
+
+        Log.i(TAG, stringBuilder.toString());
+    }
+}

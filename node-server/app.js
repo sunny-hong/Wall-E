406682a0dd2b013cfb3927cd3a9e620f0c6d95c8
sang <sangolan2@gmail.com>
2016-10-09 06:20:32 -0400

    added node-server

---
+++ node-server/app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+let commands = [];
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/post-command/:lm/:rm/:duration", (req,res)=>{
+    var command = "" + req.params.lm + req.params.rm + req.params.duration;
+    commands.push(command);
+    res.status(200).send(command);
+});
+
+app.get("/get-commands", (req,res)=>{
+  var currCommands = commands;
+  commands = [];
+  res.status(200).send(currCommands);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
